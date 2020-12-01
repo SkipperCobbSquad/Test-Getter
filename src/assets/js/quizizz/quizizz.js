60027f902749a855f52cf8fa488dd3d25bb32276
Skipper <46247235+SzymonFaligowski@users.noreply.github.com>
2020-12-01 21:15:22 +0100

    Quizizz fix

--- src/assets/js/quizizz/quizizz.js
+++ src/assets/js/quizizz/quizizz.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const quizizz = async () => {
   const browser = await puppeteer.launch({
     headless: false,
@@ -11,108 +13,45 @@ const quizizz = async () => {
       isLandscape: false,
     });
     await page.goto('https://quizizz.com/join');
-    await bar.set(30)
+    await bar.set(30);
     await page.waitForSelector('.player', {
       timeout: 0,
     });
     //Sender getterr=================================================>
     const test = await page.evaluate(() => {
-      class Encoding {
-        static encodeRaw(t, e, o = 'quizizz.com') {
-          let s = 0;
-          s = e
-            ? o.charCodeAt(0)
-            : o.charCodeAt(0) + o.charCodeAt(o.length - 1);
-          let r = [];
-          for (let o = 0; o < t.length; o++) {
-            let n = t[o].charCodeAt(0),
-              c = e ? this.safeAdd(n, s) : this.addOffset(n, s, o, 2);
-            r.push(String.fromCharCode(c));
-          }
-          return r.join('');
-        }
+      const ROOM_HASH_PATH = '/play-api/v4/checkRoom';
+      const QUESTIONS_AND_RESPONSES_PATH = '/api/main/game/';
+      const APP_PATH = '/join/';
 
-        static decode(t, e = !1) {
-          if (e) {
-            let e = this.extractHeader(t);
-            return this.decodeRaw(e, !0);
-          }
-          {
-            let e = this.decode(this.extractHeader(t), !0),
-              o = this.extractData(t);
-            return this.decodeRaw(o, !1, e);
-          }
-        }
+      const getRoomHash = async () => {
+        const linkParams = new URLSearchParams(window.location.search);
+        let roomCode = linkParams.get('gc');
 
-        static decodeRaw(t, e, o = 'quizizz.com') {
-          let s = this.extractVersion(t);
-          let r = 0;
-          (r = e
-            ? o.charCodeAt(0)
-            : o.charCodeAt(0) + o.charCodeAt(o.length - 1)),
-            (r = -r);
-          let n = [];
-          for (let o = 0; o < t.length; o++) {
-            let c = t[o].charCodeAt(0),
-              a = e ? this.safeAdd(c, r) : this.addOffset(c, r, o, s);
-            n.push(String.fromCharCode(a));
-          }
-          return n.join('');
-        }
+        if (!roomCode && window.location.pathname.includes(APP_PATH))
+          roomCode = prompt('Enter join code (XXX XXX(X))', '');
 
-        static addOffset(t, e, o, s) {
-          return 2 === s
-            ? this.verifyCharCode(t)
-              ? this.safeAdd(t, o % 2 == 0 ? e : -e)
-              : t
-            : this.safeAdd(t, o % 2 == 0 ? e : -e);
-        }
+        const init = {
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: `{"roomCode": "${roomCode}"}`,
+          method: 'POST',
+        };
 
-        static extractData(t) {
-          let e = t.charCodeAt(t.length - 2) - 33;
-          return t.slice(e, -2);
-        }
+        const roomHashResponse = await fetch(ROOM_HASH_PATH, init);
 
-        static extractHeader(t) {
-          let e = t.charCodeAt(t.length - 2) - 33;
-          return t.slice(0, e);
-        }
+        const roomHashJson = await roomHashResponse.json();
+        const roomHash = roomHashJson?.room?.hash;
+        return roomHash;
+      };
 
-        static extractVersion(t) {
-          if ('string' == typeof t && t[t.length - 1]) {
-            let e = parseInt(t[t.length - 1], 10);
-            if (!isNaN(e)) return e;
-          }
-          return null;
-        }
-
-        static safeAdd(t, e) {
-          let o = t + e;
-          return o > 65535
-            ? o - 65535 + 0 - 1
-            : o < 0
-            ? 65535 - (0 - o) + 1
-            : o;
-        }
-
-        static verifyCharCode(t) {
-          if ('number' == typeof t)
-            return !((t >= 55296 && t <= 56319) || (t >= 56320 && t <= 57343));
-        }
-      }
-
-      function GetSetData() {
-        let URL = window.location.href,
-          GameType = URL.slice(URL.search('gameType=') + 9, URL.length),
-          prevConx = localStorage.getItem('previousContext'),
-          parsedConx = JSON.parse(prevConx),
-          encodedRoomHash = parsedConx.game.roomHash,
-          roomHash = Encoding.decode(encodedRoomHash.split('-')[1]),
-          data = {
-            roomHash: roomHash,
-            type: GameType,
-          };
-        console.log(URL);
+      let data = getRoomHash().then((hash) => {
+        let URL = window.location.href;
+        const GameType = URL.slice(URL.search('gameType=') + 9, URL.length);
+        data = {
+          roomHash: hash,
+          type: GameType,
+        };
 
         let xhttp = new XMLHttpRequest();
         xhttp.open(
@@ -126,15 +65,15 @@ const quizizz = async () => {
         );
         xhttp.send(JSON.stringify(data));
         return JSON.parse(xhttp.responseText);
-      }
-
-      return GetSetData();
+      });
+      return data;
     });
     //===========================================================================>
-    bar.set(70, false)
-    return {test, error: false};
+    bar.set(70, false);
+    console.log(test);
+    return { test, error: false };
   } catch (error) {
     console.log(error);
-    return {test: {}, error}
+    return { test: {}, error };
   }
 };
